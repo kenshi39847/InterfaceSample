f737a8f72c3270751a21bf5259363dab1bbbe41b
kenshi39847 <kenshi.t.blue@gmail.com>
2023-12-13 09:04:53 +0900

    インターフェースの作成と実装

---
+++ src/chapter03/used/AddCalc.java
@@ -0,0 +1,15 @@
+package chapter03.used;
+
+/*
+ * Calculator実装クラス<br>
+ * 加算を行う
+ */
+public class AddCalc implements Calculator {
+
+	@Override
+	public Integer calc(Integer x, Integer y) {
+		// TODO 自動生成されたメソッド・スタブ
+		return x + y;
+	}
+
+}
---
+++ src/chapter03/used/Calculator.java
@@ -0,0 +1,11 @@
+package chapter03.used;
+
+/*
+ * 計算処理
+ */
+public interface Calculator {
+	/*
+	 * 計算処理を実行する
+	 */
+	Integer calc(Integer x, Integer y);
+}
